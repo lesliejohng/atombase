759fc649f0873820686ac1301bd3f5c85bd60127
lesliejohng <lesliejohng@aol.com>
2021-02-15 21:16:20 +0000

    added pytest incorrect and incomplete fen strings

--- positionValidity.py
+++ positionValidity.py
@@ -43,146 +43,143 @@ class Fen():
         self.validEPbtp = ['a3','b3','c3','d3','e3','f3','g3','h3']
         self.validEPsquares = self.validEPwtp + self.validEPbtp
 
-        # temporary to be removed upon completion of re-write
-        self.errorLog = []
+        # required
+        self.fenElementDict = {}
 
         # initial processing
-        self.fen = fen
-        self.fenElements = fen.split(' ')
-        self.fenElementDict = {}
-        self.fenElementDict['fenBoard']= self.fenElements[0]
+        if isinstance(fen, str):
+            self.fen = fen
+            self.fenElements = fen.split(' ')
+            self.fenElementDict['fenBoard']= self.fenElements[0]
 
-    #def __init__(self, fen ='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'):
-    #    self.fen = fen
-    #    self.fenElements = fen.split(' ')
-    #    self.fenBoard =self.fenElements[0]
-    #    self.errorLog = []
-    #    self.validCastling =['-','q','kq','Q','Qq','Qk','Qkq','K',
-    #                'Kq','Kkq','Kq','Kk','Kkq','KQ','KQq','KQk',
-    #                'KQkq']
-    #    self.validEPwtp = ['a6','b6','c6','d6','e6','f6','g6','h6']
-    #    self.validEPbtp = ['a3','b3','c3','d3','e3','f3','g3','h3']
-    #    self.validEPsquares = self.validEPwtp + self.validEPbtp
+            if len(self.fenElements) == 6:
+                if self.identifyToPlay(self.fenElements[1]):
+                    self.fenElementDict['fenToPlay'] = self.fenElements[1]
 
-        if len(self.fenElements) == 6:
-            if self.testToPlay(self.fenElements[1]):
-                self.fenElementDict['fenToPlay'] = self.fenElements[1]
-            #else:
-                #self.fenToPlay = 'unknown'
+                if self.identifyCastling(self.fenElements[2]):
+                    self.fenElementDict['fenCastling'] = self.fenElements[2]
 
-            if self.testCastling(self.fenElements[2]):
-                self.fenElementDict['fenCastling'] = self.fenElements[2]
-            #else:
-                #self.fenCastling = 'unknown'
+                if self.testEP(self.fenElements[3]):
+                    self.fenElementDict['fenEP'] = self.fenElements[3]
 
-            if self.testEP(self.fenElements[3]):
-                self.fenElementDict['fenEP'] = self.fenElements[3]
-            #else:
-                #self.fenEP = 'unkonwn'
+                if self.fenElements[4].isdigit():
+                    self.fenElementDict['fenHalfMoveClock'] = self.fenElements[4]
+                else:
+                    # as uncritical reset to 0
+                    self.fenElementDict['fenHalfMoveClock'] = '0'
 
-            if self.fenElements[4].isdigit():
-                self.fenElementDict['fenHalfMoveClock'] = self.fenElements[4]
+                if self.fenElements[5].isdigit():
+                    self.fenElementDict['fenMoveCounter'] = self.fenElements[5]
+                else:
+                    # as uncritical reset to 1
+                    self.fenElementDict['fenMoveCounter'] = '1'
             else:
-                # as uncritical reset to 0
-                self.fenElementDict['fenHalfMoveClock'] = '0'
+                self.message = WarningMsg(header = 'Fen Error',
+                    body = 'incomplete fen string',
+                    instruction = 'please check fen has all the required elements')
+                self.message
+                self.partFenInput()
 
-            if self.fenElements[5].isdigit():
-                self.fenElementDict['fenMoveCounter'] = self.fenElements[5]
-            else:
-                # as uncritical reset to 1
-                self.fenElementDict['fenMoveCounter'] = '1'
         else:
-            # critical information
-            #self.fenToPlay = 'unknown'
-            # critical information
-            #self.fenCastling = 'unknown'
-            # critical information
-            #self.fenEP = 'unknown'
-            # non-critical reset to 0
-            self.fenElementDict['fenHalfMoveClock'] = '0'
-            # non-critical reset to 1
-            self.fenElementDict['fenMoveCounter'] = '1'
-
             self.message = WarningMsg(header = 'Fen Error',
-                body = 'incomplete fen string',
-                instruction = 'please check fen has all the required elements')
-            self.errorLog.append('fenError')
+                    body = 'input is not a string',
+                    instruction = 'please re-input')
             self.message
+            self.fullFenInput()
 
         self.testBoard()
-        #self.fenErrorFlagCheck()
-        #self.fenIncorrectFlagCheck()
 
-        def __repr__(self):
-         return self.fen
+    def __repr__(self):
+        return self.fen
 
+    # This section is for routines which identify fenElements
+
+    def identifyToPlay(self, fenToPlay):
+        # ToPlay consists of a single 'w' or 'b' character
+        if fenToPlay == 'w' or fenToPlay == 'b':
+            return True
+        else:
+            self.message = WarningMsg(header = 'Error in To Play element of fen',
+                    body = str(fenToPlay)+' input is not valid',
+                    instruction = 'should be either "w" or "b". Please re-input.')
+            self.message
+            self.toPlayInput()
+            return True
+
+    def identifyCastling(self,fenCastling):
+        if not 'fenToPlay' in self.fenElementDict.keys():
+            self.toPlayInput()
+
+        if fenCastling in self.validCastling:
+            return True
+        else:
+            self.message = WarningMsg(header = 'Castling: '+str(fenCastling),
+                    body = 'The Castling Element is not in a valid form',
+                    instruction = 'format "-" or up to 4 letters in order KQkq')
+            self.message
+            self.castlingInput()
+            return False
+
+    # This section is for input routines
+
+    def partFenInput(self):
+        # case of incomplete fen
+        # temporary
+        # sets all but fenBoard as if it ws the starting position
+        self.fenElementDict['fenToPlay'] = 'w'
+        self.fenElementDict['fenCastling'] = 'KQkq'
+        self.fenElementDict['fenEP'] = '-'
+        self.fenElementDict['fenHalfMoveClock'] = '0'
+        self.fenElementDict['fenMoveCounter'] = '1'
+
+    def fullFenInput(self):
+        # temporary
+        # set these values
+        self.fenElementDict['fenBoard'] = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+        self.fenElementDict['fenToPlay'] = 'w'
+        self.fenElementDict['fenCastling'] = 'KQkq'
+        self.fenElementDict['fenEP'] = '-'
+        self.fenElementDict['fenHalfMoveClock'] = '0'
+        self.fenElementDict['fenMoveCounter'] = '1'
+
+    def toPlayInput(self):
+        # temporary
+        # set ToPlay to white
+        self.fenElementDict['fenToPlay'] = 'w'
+
+    def castlingInput(self):
+        # temporary
+        # set castling to call
+        self.fenElementDict['fenCastling'] = 'KQkq'
+
+    # This section is for deeper validity checks
     def testBoard(self):
-    # this is a series of tests on the board to see if it valid
+        # this is a series of tests on the board to see if it valid
 
-    # test that there are two kings on the fenBoard
-    # one White and one Black
+        # test that there are two kings on the fenBoard
+        # one White and one Black
         self.whiteKing = self.fenElementDict['fenBoard'].count('K')
         self.blackKing = self.fenElementDict['fenBoard'].count('k')
 
         if self.whiteKing == 0:
             self.message = WarningMsg(header = 'Illegal Position',
                 body = 'There is no White King on the board')
-            self.errorLog.append('noWhiteKing')
             self.message
 
         if self.whiteKing > 1:
             self.message = WarningMsg(header = 'Illegal Position',
                 body = 'There are too many White Kings on the Board')
-            self.errorLog.append('manyWhiteKings')
             self.message
 
         if self.blackKing == 0:
             self.message = WarningMsg(header = 'Illegal Position',
                 body = 'There is no Black King on the board')
-            self.errorLog.append('noBlackKing')
             self.message
 
         if self.blackKing > 1:
             self.message = WarningMsg(header = 'Illegal Position',
                 body = 'There are too many Black Kings on the board')
-            self.errorLog.append('manyBlackKings')
             self.message
-
-    def testToPlay(self,fenToPlay):
-        if len(fenToPlay) == 1:
-            if fenToPlay in 'wb':
-                return True
-            else:
-                self.message = WarningMsg(header = 'Insufficient Information',
-                    body = 'Not clear whether it is White or Black to play',
-                    instruction = 'format lowercase "w" or "b"')
-                self.errorLog.append('fenToPlayError')
-                self.message
-                return False
-        else:
-            self.message = WarningMsg(header = '"To Play" element of fen',
-                    body = 'This element is too long')
-            self.errorLog.append('fenToPlayLength')
-            self.message
-            return False
-
-    def testCastling(self,fenCastling):
-        if len(fenCastling) < 5:
-            if fenCastling in self.validCastling:
-                return True
-            else:
-                self.message = WarningMsg(header = 'Castling',
-                    body = 'The Castling Element is not in a valid form',
-                    instruction = 'format "-" or up to 4 letters in order KQkq')
-                self.errorLog.append('fenCastlingError')
-                self.message
-                return False
-        else:
-            self.message = WarningMsg(header = '"Castling" element of fen',
-                body = 'This element is too long')
-            self.errorLog.append('fenCastlingLength')
-            self.message
-            return False
 
     def testEP(self,fenEP):
         if fenEP == '-' or len(fenEP) == 2:
@@ -195,9 +192,8 @@ class Fen():
                         if fenEP in self.validEPwtp:
                             return True
                         else:
-                            self.message = WarningMsg(header = 'EP square' + fenEP,
+                            self.message = WarningMsg(header = 'EP square: ' + str(fenEP),
                                 body = 'The EP square is not valid in a "white to play" position')
-                            self.errorLog.append('fenEPSquareInvalid')
                             self.message
                             return False
 
@@ -205,76 +201,28 @@ class Fen():
                         if fenEP in self.validEPbtp:
                             return True
                         else:
-                            self.message = WarningMsg(header = 'EP square' + fenEP,
+                            self.message = WarningMsg(header = 'EP square' + str(fenEP),
                                 body = 'The EP square is not valid in a "black to play" position')
-                            self.errorLog.append('fenEPSquareInvalid')
                             self.message
                             return False
                 else:
-                    self.message = WarningMsg(header = 'EP '+ fenEP,
+                    self.message = WarningMsg(header = 'EP '+ str(fenEP),
                         body = 'The EP Element is not a valid square')
-                    self.errorLog.append('fenEPSquareInvalid')
                     self.message
                     return False
 
                 if self.fenElementDict.get('fenToPlay' ,'unknown') == 'unknown':
-                    self.message = WarningMsg(header = 'EP square ' + fenEP,
+                    self.message = WarningMsg(header = 'EP square ' + str(fenEP),
                         body = 'It is not known who it is to play',
                         instruction = 'It is not possible to check the validity of the EP square')
-                    self.errorLog.append('fenEPSquareUnclear')
                     self.message
                     return False
 
         else:
             self.message = WarningMsg(header = '"EP" element of fen',
                 body = 'This element is too long')
-            self.errorLog.append('fenEPLength')
             self.message
             return False
-
-    #def fenIncorrectFlagCheck(self):
-        # check that fenBoard, fenHalfMoveClock, fenMoveCounter
-        # have not been set to 'unknown'
-    #    self.OKflag = True
-    #    if self.fenElementDict['fenBoard' , 'unknown'] == 'unknown':
-    #        self.errorLog.append('incorrectBoardFlag')
-    #        self.message = WarningMsg(header = 'Error ' + fenEP,
-    #            body = 'The fenBoard element has been set to "unknown"',
-    #            instruction = 'This should never happen as this looses too much information')
-    #        self.OKflag = False
-    #    if not self.fenElementDict['fenHalfMoveClock'].isdigit():
-    #        self.errorLog.append('incorrectHalfMoveFlag')
-    #        self.message = WarningMsg(header = 'Error' + fenEP,
-    #            body = 'The fenHalfMoveClock clock element has been set to "unknown"',
-    #            instruction = 'If incorrect in original fen the figure should be reset to 0')
-    #        self.OKflag = False
-    #    if not self.fenElementDict['fenMoveCounter'].isdigit():
-    #        self.errorLog.append('incorrectMoveFlag')
-    #        self.message = WarningMsg(header = 'Error ' + fenEP,
-    #            body = 'The fenMove Counter has been set to "unknown"',
-    #            instruction = 'It incorrect in original fen the figure should be reset to 1')
-    #        self.OKflag = False
-    #    return self.OKflag
-
-    #def fenErrorFlagCheck(self):
-        #self.unknownCount = 0
-        # check whether fenError flag is correct
-        # if none of the elements are flagged as 'unknown'
-        # then the flag is incorrect
-        #self.fenIncorrectFlagCheck()
-
-        #if 'fenError' in self.errorLog:
-            # fenBoard is not checked as this should never
-            # be set to 'unknown'
-            #if self.fenToPlay == 'unknown':
-                #self.unknownCount += 1
-            #if self.fenCastling == 'unknown':
-                #self.unknownCount += 1
-            #if self.fenEP == 'unknown':
-                #self.unknownCount += 1
-            #if self.unknownCount == 0:
-                #self.errorLog.remove('fenError')
-            # fenHalfMoveClock and fenMoveCounter should always be a digit
 
 # initial test
 # test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq e3 1 2')
--- test_fen.py
+++ test_fen.py
@@ -11,101 +11,91 @@ def good_ep_fen():
     # sets up a Fen object with a valid EP square
     return Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq e3 1 2')
 
-def test_insufficient_fen():
-    # this makes sure that I have not added anything to the program
-    # which breaks my rules that
-    #   1 The fenBoard element should never be set to 'unknown'
-    #   2 A problem with the fenHalfMoveClock element should just result
-    #       in the value being reset to 0
-    #   3 A problem with the fenHalfMoveClock element should just result
-    #       in the value being reset to 0
+def test_missingFen():
+    # temporary
+    # full reset to starting position
+    test = Fen()
+    assert test.fenElementDict.get('fenBoard') == 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+    assert test.fenElementDict.get('fenToPlay') == 'w'
+    assert test.fenElementDict.get('fenCastling') == 'KQkq'
+    assert test.fenElementDict.get('fenEP') == '-'
+    assert test.fenElementDict.get('fenHalfMoveClock') == '0'
+    assert test.fenElementDict.get('fenMoveCounter') == '1'
+
+def test_nonStringFen():
+    # temporary
+    # full reset to starting position
+    test = Fen(5)
+    assert test.fenElementDict.get('fenBoard') == 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+    assert test.fenElementDict.get('fenToPlay') == 'w'
+    assert test.fenElementDict.get('fenCastling') == 'KQkq'
+    assert test.fenElementDict.get('fenEP') == '-'
+    assert test.fenElementDict.get('fenHalfMoveClock') == '0'
+    assert test.fenElementDict.get('fenMoveCounter') == '1'
+
+def test_insufficientFen():
+    # temporary
+    # fen Board is as given
+    # other elements reset to those in the starting position
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R KQkq - 1 2')
-    assert test.errorLog == ['fenError']
-    assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
-    assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
-    assert test.fenElementDict.get('fenMoveCounter').isdigit()
+    assert test.fenElementDict.get('fenBoard') == 'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R'
+    assert test.fenElementDict.get('fenToPlay') == 'w'
+    assert test.fenElementDict.get('fenCastling') == 'KQkq'
+    assert test.fenElementDict.get('fenEP') == '-'
+    assert test.fenElementDict.get('fenHalfMoveClock') == '0'
+    assert test.fenElementDict.get('fenMoveCounter') == '1'
 
 def test_noWhiteKing():
     # this checks that the absence of a white king results in an error
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQQB1R b KQkq - 1 2')
-    assert test.errorLog == ['noWhiteKing']
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_manyWhiteKings():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKK1R b KQkq - 1 2')
-    assert test.errorLog == ['manyWhiteKings']
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_noBlackKing():
     test = Fen('rnbqqbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2')
-    assert test.errorLog == ['noBlackKing']
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_manyBlackKings():
     test = Fen('rnbqkknr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2')
-    assert test.errorLog == ['manyBlackKings']
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_toPlayError():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R z KQkq - 1 2')
-    assert test.errorLog == ['fenToPlayError']
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
-def test_toPlayLength():
-    test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R bb KQkq - 1 2')
-    assert test.errorLog == ['fenToPlayLength']
-    assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
-    assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
-    assert test.fenElementDict.get('fenMoveCounter').isdigit()
-
-def test_CastlingCharacters():
+def test_CastlingError():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkx - 1 2')
-    assert test.errorLog == ['fenCastlingError']
-    assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
-    assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
-    assert test.fenElementDict.get('fenMoveCounter').isdigit()
-
-def test_CastlingOrder():
-    test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQqk - 1 2')
-    assert test.errorLog == ['fenCastlingError']
-    assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
-    assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
-    assert test.fenElementDict.get('fenMoveCounter').isdigit()
-
-def test_CastlingLength():
-    test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkqx - 1 2')
-    assert test.errorLog == ['fenCastlingLength']
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_EPLength():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq ee6 1 2')
-    assert test.errorLog == ['fenEPLength']
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_EPNone():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2')
-    assert test.errorLog == []
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_EPUnknown():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R - KQkq e6 1 2')
-    assert test.errorLog == ['fenToPlayError', 'fenEPSquareUnclear']
     assert test.fenElementDict.get('fenBoard', 'unknown')!= 'unknown'
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
@@ -113,70 +103,60 @@ def test_EPUnknown():
 
 def test_EPInvalidSquare():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq e5 1 2')
-    assert test.errorLog == ['fenEPSquareInvalid']
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_EPwtpValid():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq e6 1 2')
-    assert test.errorLog == []
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_EPbtpValid():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq e3 1 2')
-    assert test.errorLog == []
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_EPwtpInvalid():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq e3 1 2')
-    assert test.errorLog == ['fenEPSquareInvalid']
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_EPbtpInvalid():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq e6 1 2')
-    assert test.errorLog == ['fenEPSquareInvalid']
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_validHalfMove():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 3 5')
-    assert test.errorLog == []
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock') == '3'
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_invalidHalfMove():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - x 5')
-    assert test.errorLog == []
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock') == '0'
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
 
 def test_validMoveCount():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2')
-    assert test.errorLog == []
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter') == '2'
 
 def test_invalidMoveCount():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 x')
-    assert test.errorLog == []
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter') == '1'
 
 def test_noError():
     test = Fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2')
-    assert test.errorLog == []
     assert test.fenElementDict.get('fenBoard', 'unknown') != 'unknown'
     assert test.fenElementDict.get('fenHalfMoveClock').isdigit()
     assert test.fenElementDict.get('fenMoveCounter').isdigit()
